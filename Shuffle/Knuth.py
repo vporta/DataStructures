8ff629ffd79dcd100dcfc921aec251f1582e431e
vporta <vporta7@gmail.com>
2020-03-25 21:15:32 -0400

    graham scan

---
+++ Shuffle/Knuth.py
@@ -0,0 +1,36 @@
+"""
+Knuth.py 
+The Knuth (or Fisher-Yates) shuffling algorithm guarantees
+ *  to rearrange the elements in uniformly random order, under
+ *  the assumption that Math.random() generates independent and
+ *  uniformly distributed numbers between 0 and 1.
+"""
+import random  
+import math as m 
+class Knuth:
+
+    @staticmethod
+    def shuffle(a: list):
+        n = len(a)
+        for i in range(n):
+            # choose index uniformly in [0, i]
+            r = int(random.random() * (i + 1))
+            Knuth._exch(a, i, r)
+
+    @staticmethod
+    def _exch(a, i, r):
+        a[i], a[r] = a[r], a[i]
+
+
+def main():
+    a = [1, 2, 3, 4, 5]
+    print(a)
+    Knuth.shuffle(a)
+    print(a)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
